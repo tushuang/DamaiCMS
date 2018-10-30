790340f813005806c13989dc8d9edbc75a4a1505
tushuang <1434485764@qq.com>
2018-10-30 16:54:13 +0800

    实现了非对称加密

--- back-end/controller/admin.js
+++ back-end/controller/admin.js
@@ -1,7 +1,8 @@
 const admin_model = require('../models/admin')
 const handleData = require('../util/handleData')
 var jwt = require('jsonwebtoken');
-
+const fs = require('fs')
+const PATH = require('path')
 const signup = async (req,res,next)=>{
     //将数据传入数据库进行比对 如果没有重复则存入数据
     // res.set('content-type', 'application/json; charset=utf8')
@@ -38,13 +39,19 @@ const signin = async (req,res,next)=>{ // 登录
         if(isRight){
             // 登录成功后存一个session 退出时删除
             // 对称 加密
-            console.log( _isRepeat,5559 )
-            let _payload = { // 钥加密的数据
+            // let _payload = { // 钥加密的数据
+            //     userid: _isRepeat[0]._id,
+            //     username: _isRepeat[0].name
+            // }
+            // let _cert = 'i love u' // 密钥
+            // var _token = jwt.sign(_payload, _cert);
+
+             let _payload = { // 钥加密的数据
                 userid: _isRepeat[0]._id,
                 username: _isRepeat[0].name
             }
-            let _cert = 'i love u' // 密钥
-            var _token = jwt.sign(_payload, _cert);
+            const privateKey = fs.readFileSync(PATH.resolve(__dirname,'../key/private.key'))
+            const _token = jwt.sign(_payload, privateKey, { algorithm: 'RS256' })
             res.cookie('token', _token)  // 加密后 直接给前端设置一个cookie
             res.render('admin',{
                 code:200, // 
--- back-end/middleware/index.js
+++ back-end/middleware/index.js
@@ -1,4 +1,6 @@
 const jwt = require('jsonwebtoken')
+const fs = require('fs')
+const PATH = require('path')
 
 const setResHeader = (req,res,next)=>{
     res.set('content-type', 'application/json; charset=utf8')
@@ -7,10 +9,24 @@ const setResHeader = (req,res,next)=>{
 
 const userSigninAuth = (req,res,next)=>{
     try{
-        var decoded = jwt.verify(req.cookies.token, 'i love u'); 
+
+        // 获取验证 JWT 时需要用的公钥
+        const publicKey = fs.readFileSync(PATH.resolve(__dirname,'../key/public.key'))
+
+        // 验证 Token
+        jwt.verify(req.cookies.token, publicKey, (error, decoded) => {
+            if (error) {
+                console.log(error.message)
+                return
+            }
+            console.log(decoded,333)
+            req.token = decoded
+            next()
+        })
+        // 对称加密
+        //var decoded = jwt.verify(req.cookies.token, 'i love u'); 
         // 解密后将东西挂在req上
-        req.token = decoded
-        next()
+        
     }catch(e){
         res.render('user',{ code:403,data:JSON.stringify('登录可能过期请重新登录')})
     }
